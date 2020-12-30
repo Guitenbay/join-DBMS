4929cb56896f2bc38da7cca80b0c0f7f7afb4343
Guitenbay <gtenbay@163.com>
2020-12-30 15:12:58 +0800

    relayout

---
+++ src/algorithm/impl/hash/HashNode.java
@@ -0,0 +1,23 @@
+package algorithm.impl.hash;
+
+public class HashNode {
+    private Object data;
+    private HashNode next;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public HashNode getNext() {
+        return next;
+    }
+
+    public void setNext(HashNode next) {
+        this.next = next;
+    }
+}
+
